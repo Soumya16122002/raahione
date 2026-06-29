b7b82c0dd5b8d1bd10f915511e78ff90b2e665e2
Soumya <soumyaghosh30912@gmail.com>
2026-06-29 23:11:36 +0530

    Update API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://raahione-backend-production-321b.up.railway.app/",
+  baseURL: "https://raahione-backend-production-321b.up.railway.app",
 });
 
 export default api;
